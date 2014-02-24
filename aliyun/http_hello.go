618150c309265e534b67a026bae39ca1d2365ee8
root <root@AY140221112754199c3aZ.(none)>
2014-02-24 10:17:56 +0800

    finish handshake

--- aliyun/http_hello.go
+++ aliyun/http_hello.go
@@ -13,7 +13,7 @@ import (
          )
 
 const (
-   TOKEN = "astaxiefromqqweixin"
+   TOKEN = "every_date_love_letter"
    Text = "text"
    Location = "location"
    Image = "image"
@@ -55,17 +55,29 @@ func Signature(timestamp, nonce string) string {
    return fmt.Sprintf("%x", h.Sum(nil))
 }
 
-func getReqHeader_stub(str string) string{
-  return str
+func getReqHeader(str string, r * http.Request) string{
+  r.ParseForm()
+  for k, v := range r.Form {
+          if str == k {
+            return strings.Join(v, "")
+          }
+  }
+  return " "
 }
 func handShakeGet(w http.ResponseWriter, r * http.Request) {
-   signature := getReqHeader_stub("signature")
-   timestamp := getReqHeader_stub("timestamp")
-   nonce     := getReqHeader_stub("nonce")
-   echostr   := getReqHeader_stub("echostr")
+   signature := getReqHeader("signature", r)
+   log.Println("signature is ", signature)
+   timestamp := getReqHeader("timestamp", r)
+   log.Println("timestamp is ", timestamp)
+   nonce     := getReqHeader("nonce", r)
+   log.Println("nonce is ", nonce)
+   echostr   := getReqHeader("echostr", r)
+   log.Println("echostr is ", echostr)
    if Signature(timestamp, nonce) == signature {
       io.WriteString(w, echostr)
+      log.Println("write echostr to requestor")
    }else{
+      log.Println("signature isn't right")
       io.WriteString(w, " ")
    }
 }
@@ -80,26 +92,37 @@ func DecodeRequest(data []byte) (req *Request, err error) {
 }
 
 func handlerPost(w http.ResponseWriter, r * http.Request) {
-   
+    log.Println("entry handlerPost")
+    r.ParseForm()       //解析url传递的参数，对于POST则解析响应包的主体（request body）
+    //注意:如果没有调用ParseForm方法，下面无法获取表单的数据
+    log.Println(r.Form) //这些信息是输出到服务器端的打印信息
+    log.Println("path", r.URL.Path)
+    log.Println("scheme", r.URL.Scheme)
+    log.Println(r.Form["url_long"])
+    for k, v := range r.Form {
+        log.Println("key:", k)
+        log.Println("val:", strings.Join(v, ""))
+    }
+
 }
 func weixinhandler( w http.ResponseWriter, r * http.Request) {
    // io.WriteString(w, "Hello, world!")
-   log.Println("method is :", r.Method)
-   if r.Method == "GET" {
-    r.ParseForm()       //解析url传递的参数，对于POST则解析响应包的主体（request body）
+    log.Println("method is :", r.Method)
+    if r.Method == "GET" {
+      r.ParseForm()       //解析url传递的参数，对于POST则解析响应包的主体（request body）
     //注意:如果没有调用ParseForm方法，下面无法获取表单的数据
-    fmt.Println(r.Form) //这些信息是输出到服务器端的打印信息
-    fmt.Println("path", r.URL.Path)
-    fmt.Println("scheme", r.URL.Scheme)
-    fmt.Println(r.Form["url_long"])
-    for k, v := range r.Form {
-        fmt.Println("key:", k)
-        fmt.Println("val:", strings.Join(v, ""))
-    }
+      log.Println(r.Form) //这些信息是输出到服务器端的打印信息
+      log.Println("path", r.URL.Path)
+      log.Println("scheme", r.URL.Scheme)
+      log.Println(r.Form["url_long"])
+      for k, v := range r.Form {
+          log.Println("key:", k)
+          log.Println("val:", strings.Join(v, ""))
+      }
 
     handShakeGet(w, r)
 
-    io.WriteString(w, "Hello, world!")
+    // io.WriteString(w, "Hello, world!")
    }else {
       handlerPost(w, r)
    }
@@ -107,7 +130,7 @@ func weixinhandler( w http.ResponseWriter, r * http.Request) {
 
 func main() {
    http.HandleFunc("/weixin", weixinhandler)
-   err := http.ListenAndServe(":8080", nil)
+   err := http.ListenAndServe(":80", nil)
    if err != nil {
       log.Fatal("God like")
    }
