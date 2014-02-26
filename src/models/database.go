58a1454ffd6c9730d4aa8b8e00fb6b90e5247b35
root <root@AY140221112754199c3aZ.(none)>
2014-02-26 17:22:12 +0800

    add database

--- src/models/database.go
+++ src/models/database.go
@@ -3,6 +3,7 @@ package models
 import (
     "github.com/astaxie/goredis"
     "fmt"
+    "sync"
 )
 
 const (
@@ -15,75 +16,96 @@ const (
     ORIGINAL_LETTER         = "original_leter:content"
     SEND_ORIGINAL_LETTER    = "send_original_letter:content"
 
+    LETTER_INDEX            = "letter_index"
     FW_LETTER_INDEX         = "fw_letter:index"
     ORIGINAL_LETTER_INDEX   = "original_leter:index"
 )
+
+
+var ( 
+        mutex sync.Mutex
+    )
 type dbClient struct {
     grClient goredis.Client
 }
 
 func NewWebChatDbClient()(client *dbClient){
     client = &dbClient{}
+}
+func DBInit() {
+    client = &dbClient{}
     client.Addr = "127.0.0.1:6379"
-    val, err = client.Get(FW_LETTER_INDEX)
+    client.Db   = 13
+    return
+}
+
+func getLetterIndex(c *dbClient, feild string ) (int64, error){
+    mutex.Lock()
+    defer mutex.Unlock()
+    return c.goredis.Hincrby(LETTER_INDEX, feild, 1)
+}
+
+
+func (c *dbClient)AddSubscriber(subscriberKey string) error{
+    ismem, err := c.goredis.Sismember(UNSUBSCRIBE_USER, subscriberKey)
     if err != nil {
-        client.Set(FW_LETTER_INDEX, 0)
-        val = 0
+        return err
     }
-    fmt.Println("fw index is ", val)
+    if ismem {
+       _, err =  c.goredis.Smove(UNSUBSCRIBE_USER, ALL_USER, subscriberKey )
+       return err
+    }
 
-    _, err = client.Get(ORIGINAL_LETTER_INDEX)
+    _, err = c.goredis.Sadd(ALL_USER, subscriber)
+    return err
+}
+
+func (c *dbClient)DelSubscriber(subscriberKey string) err error{
+    _, err =  c.goredis.Smove(ALL_USER,UNSUBSCRIBE_USER, subscriberKey )
+}
+
+func (c *dbClient)AddForwardLetter(letter string) err error{
+    index , err := getLetterIndex(c, FW_LETTER_INDEX)
     if err != nil {
-        client.Set(ORIGINAL_LETTER_INDEX, 0)
-        val = 0
+        return 
     }
-    fmt.Println("origin index is ", val)
-
-    return
-}
-func dbInit() {
-    
-}
-
-func (client dbClient)AddSubscriber(subscriberKey string) err error{
-    err = nil
+    _, err = c.goredis.Sadd(FW_LETTER, index)
+    if err != nil {
+        return
+    }
+    _, err = c.goredis.HSet(FW_LETTER, index, letter)
     return
 }
 
-func (client dbClient)DelSubscriber(subscriberKey string) err error{
-    err = nil
+func (c *dbClient)DelForwardLetter(index int64) err error {
+    _, err = c.goredis.Smove(FW_LETTER, SEND_FW_LETTER, index)
     return
 }
 
-func (client dbClient)AddForwardLetter(letter string) err error{
-    err = nil
+func (c *dbClient)GetForwardLetterNum() ( int,  error)  {
+    return c.goredis.Scard(FW_LETTER)
+}
+
+func (c dbClient)AddOriginLetter(letterKey string)err error {
+    index , err := getLetterIndex(c, ORIGINAL_LETTER_INDEX)
+    if err != nil {
+        return 
+    }
+    _, err = c.goredis.Sadd(ORIGINAL_LETTER, index)
+    if err != nil {
+        return
+    }
+    _, err = c.goredis.HSet(ORIGINAL_LETTER, index, letter)
     return
 }
 
-func (client dbClient)DelForwardLetter(letterKey string) err error {
-    err = nil
+func (c dbClient)DelOriginLetter(letterKey string) err error {
+    _, err = c.goredis.Smove(ORIGINAL_LETTER, SEND_ORIGINAL_LETTER, index)
     return
 }
 
-func (client dbClient)GetForwardLetterNum() (num int, err error)  {
-    num = 0
-    err = nil
-    return
-}
-
-func (client dbClient)AddOriginLetter(letterKey string)err error {
-    err = nil
-    return
-}
-
-func (client dbClient)DelOriginLetter(letterKey string) err error {
-    err = nil
-    return
-}
-
-func (client dbClient)GetOriginLetterNum()(num int, err error) {
-    err = nil
-    return
+func (c dbClient)GetOriginLetterNum()(num int, err error) {
+    return c.goredis.Scard(ORIGINAL_LETTER)
 }
 
 
